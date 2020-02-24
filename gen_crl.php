3ffc28d4f9b8d064bacaf9d39d0a0dc93a609468
John Crisp <john@reetspetit.net>
2020-02-24 15:38:00 +0100

    add gen_crl.php for cron based crl updates

---
+++ gen_crl.php
@@ -0,0 +1,14 @@
+<?php
+/* Generate CRLs from cron
+ * Add a link to your cron to automagically update the CRL
+ */
+ 
+include('../html/config.php');
+include(STORE_DIR.'/config/config.php');
+include('../html/include/my_functions.php');
+include('../html/include/common.php') ;
+include('../html/include/openssl_functions.php') ;
+
+CA_generate_crl();
+
+?>
