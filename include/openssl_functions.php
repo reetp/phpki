1a148b36403ee1bc8ce23df18134798f20e3e8b5
John Crisp <jcrisp@safeandsoundit.co.uk>
2020-03-06 13:02:02 +0100

    SHA256 not required as set by default_md

--- include/openssl_functions.php
+++ include/openssl_functions.php
@@ -596,10 +596,10 @@ function CA_create_cert($cert_type='email',$country,$province,$locality,$organiz
 	$cmd_output[] = 'Creating certificate request.';
 
 	if (($_passwd) && ($_passwd != "''")) {
-		exec(REQ." -SHA256 -new -newkey rsa:$keysize -keyout '$userkey' -out '$userreq' -config '$cnf_file' -days '$expiry_days' -passout pass:$_passwd  2>&1", $cmd_output, $ret);
+		exec(REQ." -new -newkey rsa:$keysize -keyout '$userkey' -out '$userreq' -config '$cnf_file' -days '$expiry_days' -passout pass:$_passwd  2>&1", $cmd_output, $ret);
 	}
 	else {
-		exec(REQ." -SHA256 -new -newkey rsa:$keysize -keyout '$userkey' -out '$userreq' -config '$cnf_file' -days '$expiry_days' -nodes  2>&1", $cmd_output, $ret);
+		exec(REQ." -new -newkey rsa:$keysize -keyout '$userkey' -out '$userreq' -config '$cnf_file' -days '$expiry_days' -nodes  2>&1", $cmd_output, $ret);
 	}
 	
 	# Sign the certificate request and create the certificate
